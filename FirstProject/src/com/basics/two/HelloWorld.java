86529715a67f8fe20f182b9e4fb237c54831cf73
Jaggs26 <kvenkatajagadesh@gmail.com>
2024-08-10 12:51:38 +0530

    // hi i am jagadesh

--- FirstProject/src/com/basics/two/HelloWorld.java
+++ FirstProject/src/com/basics/two/HelloWorld.java
@@ -1,6 +1,11 @@
 package com.basics.two;
-
+import java.util.*;
 public class HelloWorld {
 // function to say hello world
+	public static void main(String[] args) {
+		
+	
 	System.out.println("Hello World");
+	System.out.println("Hi i am Jagadesh");
+	}
 }
